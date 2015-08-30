be9970bdd4776a3995d011af3844641d5f8ffbb3
Pat Wey <patrickwey@gmail.com>
2015-08-30 15:04:17 -0600

    made merge_sort not suck

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,20 +1,68 @@
-array = [2, 0, 1, 3]
+array = ['c', 'a', 'b', 'd']
 
-def split(array)
-  return array[0] if array.count == 1
-  if array.count % 2 == 0 # it has an even number of elements
-    a = []
-    b = []
-    half_size = array.count / 2
-    0.upto(half_size) do |i|
-      a << array[i]
-      split(a)
-    end
-    half_size.upto(array.count) do |i|
-      b << array[i]
-      split(b)
-    end
+def split_left(array)
+  if array.count == 2
+    left = array.slice!(0, 1)
+    right = array
+    return left, right
+  else
+    array = array.slice!(0, array.count / 2)
   end
+  split_left(array)
 end
 
-split(array) # =>
+def split_right(array)
+  if array.count == 2
+    left = array.slice!(0, 1)
+    right = array
+    return left, right
+  else
+    array = array.slice(array.count / 2, array.count)
+  end
+  split_right(array)
+end
+
+def combine(left, right, combined)
+  # combined = [] if combined.nil?
+  if left.count == 0 && right.count == 0
+    return combined
+
+  elsif left[0] && right[0] # they're not nil
+
+    if left[0] < right[0]
+      combined << left[0]
+      left.delete_at 0
+    else
+      combined << right[0]
+      right.delete_at 0
+    end
+
+  elsif left[0].nil? && right[0]
+    combined << right[0]
+    right.delete_at 0
+
+  elsif right[0].nil? && left[0]
+    combined << left[0]
+    left.delete_at 0
+
+  end
+  combine(left, right, combined)
+end
+
+left, right = split_left(array)
+left # => ["c"]
+right # => ["a"]
+
+combined = []
+combined = combine(left, right, combined)
+combined # => ["a", "c"]
+
+
+
+
+
+__END__
+
+left_r, right_r = split_right(array)
+left_r # => ["b"]
+right_r # => ["d"]
